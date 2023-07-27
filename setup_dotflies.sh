4a33d24a2a97c7c6a1011a6f93c315d62beaec35
ShinyNito <82853247+ShinyNito@users.noreply.github.com>
2023-07-27 11:56:48 +0800

    Add script to manage dotfiles setup
    The new shell script, setup_dotfiles.sh, automates the installation and updating of dotfiles in the user's home directory. It checks if the dotfiles directory is a symlink, if not, it backs up the existing file if it exists and creates a new symlink. In case the directory already exists, it updates the dotfiles from the Git repository. This script streamlines and simplifies the management of the dotfiles.

---
+++ setup_dotflies.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function manage_file {
+  local filename=$1
+
+  if [ ! -L "$HOME/$filename" ]; then
+    if [ -f "$HOME/$filename" ]; then
+      echo "Backing up existing $filename file..."
+      mv "$HOME/$filename" "$HOME/$filename.bak"
+    fi
+    echo "Creating $filename symlink..."
+    ln -s "$LOCAL_DIR/$filename" "$HOME/$filename"
+  else
+    echo "Skipping existing $filename symlink..."
+  fi
+}
+
+REPO_URL="https://github.com/ShinyNito/dotfiles"
+LOCAL_DIR="$HOME/.dotfiles"
+
+if [ -d "$LOCAL_DIR" ]; then
+  echo "Updating dotfiles..."
+  cd "$LOCAL_DIR"
+  git pull origin master
+else
+  echo "Cloning dotfiles..."
+  git clone "$REPO_URL" "$LOCAL_DIR"
+fi
+
+manage_file ".zshrc"
+
+manage_file ".p10k.zsh"
+
+echo "Setup complete!"
+
+source ~/.zshrc
