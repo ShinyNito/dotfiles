70d4834c6b309fda0ed96c5f609ccd3509d88415
ShinyNito <82853247+ShinyNito@users.noreply.github.com>
2023-07-28 14:23:32 +0800

    Add functionality to check software installation and improve output
    This commit introduces three main changes to the setup script. Firstly, a function is added to check if a software is installed, improving the script's robustness. Secondly, output of the script is enhanced by incorporating 'gum' commands for generating spinners and more interactive messages. When 'gum' is not installed, it falls back to simple echo statements. Lastly, instead of directly executing commands, they're wrapped around 'run_cmd' function to manage its output better.

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,17 +1,55 @@
 #!/bin/bash
 
-function manage_file {
+is_installed()
+{
+  local software_name=$1
+  if command -v $software_name &> /dev/null; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+gum_spin()
+{
+  local title=$1
+  local cmd=$2
+  gum spin --spinner dot --title "$title" -- "$cmd"
+}
+
+run_cmd()
+{
+  local title=$1
+  local cmd=$2
+  if is_installed "gum"; then
+    gum_spin "$title" "$cmd"
+  else
+    echo "$title"
+    eval "$cmd"
+  fi
+}
+
+tips()
+{
+    local title="$1"
+    if is_installed "gum"; then
+      echo '{{ Color "2" "0" " '$title' " }}' | gum format -t template
+    else
+      echo "$title"
+    fi
+}
+
+manage_file()
+{
   local filename=$1
 
   if [ ! -L "$HOME/$filename" ]; then
     if [ -f "$HOME/$filename" ]; then
-      echo "Backing up existing $filename file..."
-      mv "$HOME/$filename" "$HOME/$filename.bak"
+      run_cmd "Backing up existing $filename file..." "mv $HOME/$filename $HOME/$filename.bak"
     fi
-    echo "Creating $filename symlink..."
-    ln -s "$LOCAL_DIR/$filename" "$HOME/$filename"
+    run_cmd "Creating $filename symlink..." "ln -s $LOCAL_DIR/$filename $HOME/$filename"
   else
-    echo "Skipping existing $filename symlink..."
+    tips "$filename symlink already exists. Skipping..."
   fi
 }
 
@@ -19,17 +57,14 @@ REPO_URL="https://github.com/ShinyNito/dotfiles"
 LOCAL_DIR="$HOME/.dotfiles"
 
 if [ -d "$LOCAL_DIR" ]; then
-  echo "Updating dotfiles..."
   cd "$LOCAL_DIR"
-  git pull origin main
+  gum_spin "Updating dotfiles..." "git pull origin main"
 else
-  echo "Cloning dotfiles..."
-  git clone "$REPO_URL" "$LOCAL_DIR"
+  gum_spin "Cloning dotfiles..." "git clone $REPO_URL $LOCAL_DIR"
 fi
 
 manage_file ".zshrc"
 
 manage_file ".p10k.zsh"
 
-echo "Setup complete!"
-
+tips "Done!"
